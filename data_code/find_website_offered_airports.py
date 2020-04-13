9f86cdbddd1ebef046f34551a85ad40441bb1656
Christine <czhao028@gmail.com>
2020-04-13 00:37:49 -0400

    selenium stuff starting

---
+++ data_code/find_website_offered_airports.py
@@ -0,0 +1,19 @@
+import re
+import xlrd
+
+loc = ("../excel_data/AIRPORTS_FLIGHT24.xlsx")
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
+print(sheet.nrows)
+airport_codes = set()
+for i in range(1, sheet.nrows):
+    value = sheet.cell_value(i, 0)
+    match = re.search("\((.*)\/.*\)", value)
+    if match == None:
+        break
+    g0 = match.group(1)
+
+    print(g0)
+    airport_codes.add(g0)
+
+print(len(airport_codes))
